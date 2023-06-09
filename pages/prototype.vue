a07c0bd45709a30348bae2476edf59e5db5fb2a3
Kim@PC <kimdeguzman20@yahoo.com>
2023-06-09 19:43:19 +0800

    Update prototype page

--- pages/prototype.vue
+++ pages/prototype.vue
@@ -4,106 +4,66 @@
             <v-app class="tw-font-sans">
                 <v-container>
                     <div class="tw-space-y-2">
-                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-4 2xl:tw-grid-cols-8 tw-font-medium">
+                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-5 xl:tw-grid-cols-6 2xl:tw-grid-cols-8 tw-font-medium">
                             <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
                                 <Button :height="'xs'">Authenticate</Button>
                             </div>
-
                             <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
                                 <Button :height="'sm'">Authenticate</Button>
                             </div>
-
                             <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
                                 <Button :height="'md'">Authenticate</Button>
                             </div>
-
                             <div class="tw-flex tw-items-start tw-justify-start tw-block tw-w-full tw-border tw-border-neutral-200">
                                 <Button :height="'lg'">Authenticate</Button>
                             </div>
+                            <div class="tw-col-span-2 tw-flex tw-items-start tw-justify-start tw-block tw-w-full tw-border tw-border-neutral-200">
+                                <Button :height="'xl'">Authenticate</Button>
+                            </div>
                         </div>
 
-                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-4 2xl:tw-grid-cols-8 tw-font-medium">
+                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-5 xl:tw-grid-cols-6 2xl:tw-grid-cols-8 tw-font-medium">
                             <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
                                 <label class="tw-flex tw-items-center">
-                                    <Checkbox :height="'sm'" name="remember" />
-                                    <span class="tw-ml-2 tw-text-xs">Remember me</span>
+                                    <Checkbox :height="'sm'" :label="'Remember me'" name="remember" />
                                 </label>
                             </div>
-
                             <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
                                 <label class="tw-flex tw-items-center">
-                                    <Checkbox :height="'md'" name="remember" />
-                                    <span class="tw-ml-2 tw-text-xs">Remember me</span>
+                                    <Checkbox :height="'md'" :label="'Remember me'" name="remember" />
                                 </label>
                             </div>
-
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
+                            <div class="tw-col-span-2 tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
                                 <label class="tw-flex tw-items-center">
-                                    <Checkbox name="remember" />
-                                    <span class="tw-ml-2 tw-text-xs">Remember me</span>
-                                </label>
-                            </div>
-
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
-                                <label class="tw-flex tw-items-center">
-                                    <Checkbox :height="'lg'" name="remember" />
-                                    <span class="tw-ml-2 tw-text-xs">Remember me</span>
+                                    <Checkbox :height="'lg'" :label="'Remember me'" name="remember" />
                                 </label>
                             </div>
                         </div>
 
-                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-4 2xl:tw-grid-cols-8 tw-font-medium">
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
-                                <label class="tw-flex tw-items-center tw-w-full">
-                                    <FormInputLabel for="form_input_1" value="xs" />
-                                    <FormInput :height="'xs'" class="tw-ml-0.5 tw-w-full" placeholder="placeholder" id="form_input_1" type="text" autofocus autocomplete="off" />
-                                </label>
+                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-5 xl:tw-grid-cols-6 2xl:tw-grid-cols-8 tw-font-medium">
+                            <div class="tw-block tw-border tw-border-neutral-200">
+                                <FormInputLabel :height="'xs'" for="form_input_1" value="XS Label" />
+                                <FormInput :height="'xs'" class="tw-w-full" placeholder="Enter username" id="form_input_1" type="text" autofocus autocomplete="off" />
                             </div>
-
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
-                                <label class="tw-flex tw-items-center tw-w-full">
-                                    <FormInputLabel for="form_input_2" value="sm" />
-                                    <FormInput :height="'sm'" class="tw-ml-0.5 tw-w-full" placeholder="placeholder" id="form_input_2" type="text" autofocus autocomplete="off" />
-                                </label>
+                            <div class="tw-block tw-border tw-border-neutral-200">
+                                <FormInputLabel :height="'sm'" for="form_input_2" value="SM Label" />
+                                <FormInput :height="'sm'" class="tw-w-full" placeholder="Enter username" id="form_input_2" type="text" autofocus autocomplete="off" />
                             </div>
-
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
-                                <label class="tw-flex tw-items-center tw-w-full">
-                                    <FormInputLabel for="form_input_3" value="md" />
-                                    <FormInput :height="'md'" class="tw-ml-0.5 tw-w-full" placeholder="placeholder" id="form_input_3" type="text" autofocus autocomplete="off" />
-                                </label>
+                            <div class="tw-block tw-border tw-border-neutral-200">
+                                <FormInputLabel :height="'md'" for="form_input_3" value="MD Label" />
+                                <FormInput :height="'md'" class="tw-w-full" placeholder="Enter username" id="form_input_3" type="text" autofocus autocomplete="off" />
                             </div>
-
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
-                                <label class="tw-flex tw-items-center tw-w-full">
-                                    <FormInputLabel for="form_input_4" value="lg" />
-                                    <FormInput :height="'lg'" class="tw-ml-0.5 tw-w-full" placeholder="placeholder" id="form_input_4" type="text" autofocus autocomplete="off" />
-                                </label>
+                            <div class="tw-block tw-border tw-border-neutral-200">
+                                <FormInputLabel :height="'lg'" for="form_input_4" value="LG Label" />
+                                <FormInput :height="'lg'" class="tw-w-full" placeholder="Enter username" id="form_input_4" type="text" autofocus autocomplete="off" />
                             </div>
-
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
-                                <label class="tw-flex tw-items-center tw-w-full">
-                                    <FormInputLabel for="form_input_5" value="xl" />
-                                    <FormInput :height="'xl'" class="tw-ml-0.5 tw-w-full" placeholder="placeholder" id="form_input_5" type="text" autofocus autocomplete="off" />
-                                </label>
-                            </div>
-
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200">
-                                <label class="tw-flex tw-items-center tw-w-full">
-                                    <FormInputLabel for="form_input_6" value="2xl" />
-                                    <FormInput :height="'2xl'" class="tw-ml-0.5 tw-w-full" placeholder="placeholder" id="form_input_6" type="text" autofocus autocomplete="off" />
-                                </label>
-                            </div>
-
-                            <div class="tw-flex tw-items-start tw-justify-start tw-block tw-border tw-border-neutral-200 tw-col-span-2">
-                                <label class="tw-flex tw-items-center tw-w-full">
-                                    <FormInputLabel for="form_input_7" value="3xl" />
-                                    <FormInput :height="'3xl'" class="tw-ml-0.5 tw-w-full" placeholder="placeholder" id="form_input_7" type="text" autofocus autocomplete="off" />
-                                </label>
+                            <div class="tw-col-span-2 tw-block tw-border tw-border-neutral-200">
+                                <FormInputLabel :height="'xl'" for="form_input_5" value="XL Label" />
+                                <FormInput :height="'xl'" class="tw-w-full" placeholder="Enter username" id="form_input_5" type="text" autofocus autocomplete="off" />
                             </div>
                         </div>
 
-                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-4 2xl:tw-grid-cols-8 tw-font-medium">
+                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-5 xl:tw-grid-cols-6 2xl:tw-grid-cols-8 tw-font-medium">
                             <div class="tw-block tw-border tw-border-neutral-200">
                                 <FormInputLabel for="bootstrapDatePicker" value="Date" />
                                 <FormInput
@@ -116,7 +76,7 @@
                                     :disabled="false" />
                             </div>
 
-                            <div class="tw-block tw-border tw-border-neutral-200">
+                            <div class="tw-col-span-2 tw-block tw-border tw-border-neutral-200">
                                 <FormInputLabel for="bootstrapDateTimePicker" value="DateTime" />
                                 <FormInput
                                     :height="'sm'"
@@ -145,7 +105,7 @@
                             </div>
                         </div>
 
-                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-2 xl:tw-grid-cols-4 2xl:tw-grid-cols-8 tw-font-medium">
+                        <div class="tw-grid tw-gap-2 tw-grid-cols-1 sm:tw-grid-cols-2 lg:tw-grid-cols-5 xl:tw-grid-cols-6 2xl:tw-grid-cols-8 tw-font-medium">
                             <div class="tw-block tw-border tw-border-neutral-200 tw-col-span-2">
                                 <FormInputLabel value="Static Single Select" />
                                 <SingleSelect
