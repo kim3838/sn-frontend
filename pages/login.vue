42aca1813f0022edeb92a18e3bfe4be7294f8b09
Kim@PC <kimdeguzman20@yahoo.com>
2023-06-11 16:27:32 +0800

    Login component update input and label usage

--- pages/login.vue
+++ pages/login.vue
@@ -10,19 +10,18 @@
         <div v-if="true" class="tw-flex tw-h-full tw-items-center tw-border-gray-200 tw-border">
             <form @submit.prevent="login" class="tw-w-72 tw-border tw-border-gray-200">
                 <div class="tw-block">
-                    <FormInputLabel for="email" value="Email" />
-                    <FormInput id="email" type="email" class="tw-mt-1 tw-text-sm tw-block tw-w-full" v-model="form.email" autofocus autocomplete="off" />
+                    <FormInputLabel :height="'md'" for="email" value="Email" />
+                    <FormInput :height="'md'" id="email" type="email" class="tw-w-full" v-model="form.email" autofocus autocomplete="off" />
                 </div>
 
                 <div class="tw-block tw-mt-4">
-                    <FormInputLabel for="password" value="Password" />
-                    <FormInput id="password" type="password" class="tw-mt-1 tw-text-sm tw-block tw-w-full" v-model="form.password" required autocomplete="current-password" />
+                    <FormInputLabel :height="'md'" for="password" value="Password" />
+                    <FormInput :height="'md'" id="password" type="password" class="tw-w-full" v-model="form.password" required autocomplete="current-password" />
                 </div>
 
                 <div class="tw-block tw-mt-4">
                     <label class="tw-flex tw-items-center">
-                        <Checkbox name="remember" v-model="form.remember" />
-                        <span class="tw-ml-2 tw-text-xs tw-text-gray-600">Remember me</span>
+                        <Checkbox name="remember" v-model="form.remember" :height="'md'" :label="'Remember me'" />
                     </label>
                 </div>
 
