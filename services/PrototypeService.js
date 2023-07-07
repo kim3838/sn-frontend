85296c183129d679c83e47240d198546b6372a7f
Kim@PC <kimdeguzman20@yahoo.com>
2023-07-07 20:10:08 +0800

    Service sample return $axios parameters instead of an $axios object

---
+++ services/PrototypeService.js
@@ -0,0 +1,22 @@
+export default class PrototypeService{
+
+    static selection(payload = {}) {
+
+        let itemsPerPage = (payload.itemsPerPage === undefined) ? '10' : payload.itemsPerPage;
+        let page = (payload.page === undefined) ? 1 : payload.page;
+        let filters = (payload.filters === undefined) ? '' : payload.filters;
+
+        return {
+            method: 'get',
+            url : '/api/selections/prototype?page=' + page + '&itemsPerPage=' + itemsPerPage,
+            config: {
+                params: {
+                    filters: filters
+                },
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        };
+    }
+}
