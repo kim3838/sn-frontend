c0d06b5e62eae0784d1a0ab71c888256ccf9b85b
Kim@PC <kimdeguzman20@yahoo.com>
2023-07-07 20:08:02 +0800

    Refactor Date time picker foundations

--- mixins/datetimepicker.js
+++ mixins/datetimepicker.js
@@ -1,94 +1,102 @@
 export default {
+    head: {
+        script: [
+            {src: 'js/common/moment.min.js', defer: true},
+            {src: 'js/common/jquery.min.js', defer: true},
+            {src: 'js/datetimepicker/index.js', defer: true}
+        ]
+    },
+
     mounted() {
         let that = this;
 
         console.log("DateTimePickerMixin mounted");
 
-        $('#bootstrapDatePicker').daterangepicker({
-            "singleDatePicker": true,
-            "showDropdowns": true,
-            "autoApply": true,
-            locale: {
-                format: 'YYYY-MM-DD'
-            }
-        }, function(start, end, label) {
+        setTimeout(() => {
+            console.log("DateTimePickerMixin bootstrap");
+            this.bootstrapDateTimePickers(that);
+        }, 200);
+    },
 
-            that.$store.commit('form/setFormFilterValue', {
-                key: 'date',
-                value: start.format('YYYY-MM-DD')
-            })
-        });
+    methods: {
+        bootstrapDateTimePickers(that){
+            $('#bootstrapDatePicker').daterangepicker({
+                "singleDatePicker": true,
+                "showDropdowns": true,
+                "autoApply": true,
+                locale: {
+                    format: 'YYYY-MM-DD'
+                }
+            }, function (start, end, label) {
 
-        $('#bootstrapDateTimePicker').daterangepicker({
-            "singleDatePicker": true,
-            "showDropdowns": true,
-            "timePicker": true,
-            "timePicker24Hour": true,
-            "timePickerSeconds": true,
-            "autoApply": false,
-            "autoUpdateInput": false,
-            locale: {
-                format: 'YYYY-MM-DD HH:mm:ss'
-            }
-        }).on('apply.daterangepicker', function(event, picker){
-            that.$store.commit('form/setFormFilterValue', {
-                key: 'dateTime',
-                value: picker.startDate.format('YYYY-MM-DD HH:mm:ss')
-            })
-        });
-
-        $('#bootstrapMonthPicker').daterangepicker({
-            "monthPicker": true,
-            "singleDatePicker": true,
-            "showDropdowns": true,
-            "autoApply": false,
-            "autoUpdateInput": false,
-            "locale": {
-                "format": "YYYY MMMM",
-                "monthNames": [
-                    "January",
-                    "February",
-                    "March",
-                    "April",
-                    "May",
-                    "June",
-                    "July",
-                    "August",
-                    "September",
-                    "October",
-                    "November",
-                    "December"
-                ],
-                "firstDay": 1
-            }
-        }).on('apply.daterangepicker', function(event, picker){
-
-            let picked = picker.selectedYear === undefined
-                ? picker.startDate.format('YYYY-MM')
-                : picker.selectedYear + '-' + (picker.selectedMonth + 1);
-
-            let value = that.$moment(picked, 'YYYY-MM-DD').format('YYYY-MM');
-            let label = that.$moment(picked, 'YYYY-MM').format('YYYY MMMM');
-
-            that.$store.commit('form/setFormFilterValue', {
-                key: 'monthValue',
-                value: value
+                that.$store.commit('form/setFormFilterValue', {
+                    key: 'date',
+                    value: start.format('YYYY-MM-DD')
+                })
             });
 
-            that.$store.commit('form/setFormFilterValue', {
-                key: 'monthLabel',
-                value: label
-            })
+            $('#bootstrapDateTimePicker').daterangepicker({
+                "singleDatePicker": true,
+                "showDropdowns": true,
+                "timePicker": true,
+                "timePicker24Hour": true,
+                "timePickerSeconds": true,
+                "autoApply": false,
+                "autoUpdateInput": false,
+                locale: {
+                    format: 'YYYY-MM-DD HH:mm:ss'
+                }
+            }).on('apply.daterangepicker', function (event, picker) {
+                that.$store.commit('form/setFormFilterValue', {
+                    key: 'dateTime',
+                    value: picker.startDate.format('YYYY-MM-DD HH:mm:ss')
+                })
+            });
 
-        });
-    },
-    created() {
-        console.log("DateTimePickerMixin created");
-    },
-    beforeCreate() {
-        console.log("DateTimePickerMixin before create");
-    },
-    beforeMount() {
-        console.log("DateTimePickerMixin before mount");
+            $('#bootstrapMonthPicker').daterangepicker({
+                "monthPicker": true,
+                "singleDatePicker": true,
+                "showDropdowns": true,
+                "autoApply": false,
+                "autoUpdateInput": false,
+                "locale": {
+                    "format": "YYYY MMMM",
+                    "monthNames": [
+                        "January",
+                        "February",
+                        "March",
+                        "April",
+                        "May",
+                        "June",
+                        "July",
+                        "August",
+                        "September",
+                        "October",
+                        "November",
+                        "December"
+                    ],
+                    "firstDay": 1
+                }
+            }).on('apply.daterangepicker', function (event, picker) {
+
+                let picked = picker.selectedYear === undefined
+                    ? picker.startDate.format('YYYY-MM')
+                    : picker.selectedYear + '-' + (picker.selectedMonth + 1);
+
+                let value = that.$moment(picked, 'YYYY-MM-DD').format('YYYY-MM');
+                let label = that.$moment(picked, 'YYYY-MM').format('YYYY MMMM');
+
+                that.$store.commit('form/setFormFilterValue', {
+                    key: 'monthValue',
+                    value: value
+                });
+
+                that.$store.commit('form/setFormFilterValue', {
+                    key: 'monthLabel',
+                    value: label
+                })
+
+            });
+        }
     }
 }
--- nuxt.config.js
+++ nuxt.config.js
@@ -31,11 +31,7 @@ export default {
             {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono&display=swap'},
             {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Spline+Sans+Mono:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap'}
         ],
-        script:[
-            { src: 'js/common/moment.min.js', defer: true },
-            { src: 'js/common/jquery.min.js', defer: true },
-            { src: 'js/datetimepicker/index.js', defer: true }
-        ],
+        script:[],
         bodyAttrs: {
             class: 'tw-text-accent tw-font-sans'
         }
